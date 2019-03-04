44a7d4f901159c6465b21df8aecca09f7e38e858
luozhengchao <289627502@qq.com>
2019-03-04 09:51:16 +0800

    接口：多继承

---
+++ high_study/demo01/src/main/java/com/luo/demo01/basicjava/A.java
@@ -0,0 +1,9 @@
+package com.luo.demo01.basicjava;
+
+/**
+ * @Author luozhengchao
+ * @Date 2019/3/4 9:47 AM
+ */
+public interface A {
+
+}
---
+++ high_study/demo01/src/main/java/com/luo/demo01/basicjava/C.java
@@ -0,0 +1,10 @@
+package com.luo.demo01.basicjava;
+
+/**
+ * 接口：多继承
+ * @Author luozhengchao
+ * @Date 2019/3/4 9:49 AM
+ */
+public interface C extends A,B{
+
+}
